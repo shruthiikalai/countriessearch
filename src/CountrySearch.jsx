1150e3ee7e5c4fd9efd22658b7603e3218116bc6
Kalaiyarasi Rajendran <kalaiyarasirajendran@healthifydigital.com>
2025-01-22 20:11:35 +0530

    test

--- src/CountrySearch.jsx
+++ src/CountrySearch.jsx
@@ -19,8 +19,8 @@ const CountrySearch = () => {
 
         // Transform data to ensure country.name and country.flag exist
         const transformedData = data.map((country) => ({
-          name: country.common || 'Unknown',
-          flag: country.png || '',
+          name: country.name || 'Unknown',
+          flag: country.flag || '',
         }));
 
         setCountries(transformedData);
